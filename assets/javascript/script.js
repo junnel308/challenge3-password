17d018d87e0b8783eb12681d36daebe8b9585ad0
junnel308 <junnelpadilla08@gmail.com>
2022-10-01 22:08:56 -0700

    first commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,29 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+/*
 function writePassword() {
-  var password = generatePassword();
+  var password = document.generatePassword("password")
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
+
+
+generateBtn.addEventListener("click", function(event) {
+  event.preventDefault;
+  document.getElementById('prompt1').classList.toggle ('active');
+});
+
+function changedata(parameter) {
+  if(parameter == 1) {
+    document.getElementById('prompt2').classList.opacity('0');
+
+  }
+}
