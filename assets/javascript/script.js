a6ff72b65b663af3a89abc4c4c1b90f7ad041623
junnel308 <junnelpadilla08@gmail.com>
2022-10-02 00:16:14 -0700

    changed elements in HTML

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,7 @@
 
 var generateBtn = document.querySelector("#generate");
+var optionSelect = document.querySelector("#includeLowercase");
+
 
 // Write password to the #password input
 /*
@@ -17,13 +19,10 @@ generateBtn.addEventListener("click", writePassword);
 
 
 generateBtn.addEventListener("click", function(event) {
-  event.preventDefault;
   document.getElementById('prompt1').classList.toggle ('active');
 });
 
-function changedata(parameter) {
-  if(parameter == 1) {
-    document.getElementById('prompt2').classList.opacity('0');
 
-  }
-}
+optionSelect.addEventListener("click", function(event) {
+  document.getElementById('prompt2').classList.toggle ('active');
+});
