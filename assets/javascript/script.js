79ab7c86be893d2da0dfb02475bc19dc08c89e38
junnel308 <junnelpadilla08@gmail.com>
2022-10-02 14:34:02 -0700

    added prompt elements to password gen

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,17 @@
 
 var generateBtn = document.querySelector("#generate");
-var optionSelect = document.querySelector("#includeLowercase");
+var includeCharacters = document.querySelector("#includeCharacters");
+var includeLowercase = document.querySelector("#includeLowercase");
+var includeUppercase = document.querySelector("#includeUppercase");
+var includeNumbers = document.querySelector("#includeNumbers");
+var includeSpecialCharacters = document.querySelector("#includeSpecialCharacters");
+var writePassword = document.querySelector("#password");
+var choiceSelection = document.querySelector("#choice-selection");
+
+includeLowercase = "abcdefghijklmnopqrstuvwxyz";
+includeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+includeNumbers = "0123456789";
+includeSpecialCharacters = "!@#$%^&*()+-/<>.,;:}{][";
 
 
 // Write password to the #password input
@@ -17,12 +28,31 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 */
 
-
-generateBtn.addEventListener("click", function(event) {
-  document.getElementById('prompt1').classList.toggle ('active');
+generateBtn.addEventListener("click", function(event){
+  document.getElementById('footer-prompt').classList.toggle('active');
 });
 
+choiceSelection.addEventListener('submit', function(event) {
+  var includeCharacters= includeCharacters.value;
+  var includeLowercase = includeLowercase.checked;
+  var includeUppercase = includeUppercase.checked;
+  var includeNumbers = includeNumbers.checked;
+  var includeSpecialCharacters = includeSpecialCharacters.checked;
+  var password = document.generatePassword(includeLowercase, includeUppercase, includeNumbers, includeSpecialCharacters, includeCharacters);
+  writePassword.value = password
+})
 
-optionSelect.addEventListener("click", function(event) {
-  document.getElementById('prompt2').classList.toggle ('active');
-});
+function generatePassword(includeLowercase, includeUppercase, includeNumbers, includeSpecialCharacters, includeCharacters) {
+  var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+/=_[]{}|;:"
+  if(includeLowercase) char = char.includeLowercase
+  if(includeUppercase) char = char.includeUppercase
+  if(includeNumbers) char = char.includeNumbers
+  if(includeSpecialCharacters) char = char.includeSpecialCharacters
+
+  var passwordText = []
+  for (var i = 0; i < characterLength; i++) {
+    var charRandom = char[Math.floor(Math.random() * characterLength)]
+    passwordText.push(String.fromCharCode(char))
+  }
+  
+}
