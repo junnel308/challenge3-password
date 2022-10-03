06bd63753c67ee3349b341a350a0ba886c9aeeca
junnel308 <junnelpadilla08@gmail.com>
2022-10-02 19:51:27 -0700

    changed elements to jacascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,58 +1,101 @@
 
 var generateBtn = document.querySelector("#generate");
-var includeCharacters = document.querySelector("#includeCharacters");
-var includeLowercase = document.querySelector("#includeLowercase");
-var includeUppercase = document.querySelector("#includeUppercase");
-var includeNumbers = document.querySelector("#includeNumbers");
-var includeSpecialCharacters = document.querySelector("#includeSpecialCharacters");
-var writePassword = document.querySelector("#password");
-var choiceSelection = document.querySelector("#choice-selection");
 
-includeLowercase = "abcdefghijklmnopqrstuvwxyz";
-includeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-includeNumbers = "0123456789";
-includeSpecialCharacters = "!@#$%^&*()+-/<>.,;:}{][";
-
-
+var charLowercase = "abcdefghijklmnopqrstuvwxyz";
+var charUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charNumbers = "0123456789";
+var charSpecialCharacters = "'!#$%&()*+,-./:;<=>?@[]^_`{|}~'";
 // Write password to the #password input
-/*
 function writePassword() {
-  var password = document.generatePassword("password")
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-*/
 
-generateBtn.addEventListener("click", function(event){
-  document.getElementById('footer-prompt').classList.toggle('active');
-});
+function generatePassword() {
+  let charLength = prompt("How many characters would you like to include? (Minimum of 8 character and Maximum of 128)")
 
-choiceSelection.addEventListener('submit', function(event) {
-  var includeCharacters= includeCharacters.value;
-  var includeLowercase = includeLowercase.checked;
-  var includeUppercase = includeUppercase.checked;
-  var includeNumbers = includeNumbers.checked;
-  var includeSpecialCharacters = includeSpecialCharacters.checked;
-  var password = document.generatePassword(includeLowercase, includeUppercase, includeNumbers, includeSpecialCharacters, includeCharacters);
-  writePassword.value = password
-})
+  let choice1 = prompt("Would you like to include lowercase letters?");
 
-function generatePassword(includeLowercase, includeUppercase, includeNumbers, includeSpecialCharacters, includeCharacters) {
-  var char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+/=_[]{}|;:"
-  if(includeLowercase) char = char.includeLowercase
-  if(includeUppercase) char = char.includeUppercase
-  if(includeNumbers) char = char.includeNumbers
-  if(includeSpecialCharacters) char = char.includeSpecialCharacters
+    if (choice1 === "yes") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Added!");
+      } else {
+        alert("Not Added!");
+      }
+    } else if (choice1 === "no") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Not Added!");
+      } else {
+        alert("Added!");
+      }
+    } else  {
+      alert("Please input 'yes' or 'no'. Try Again!");
+    }
 
-  var passwordText = []
-  for (var i = 0; i < characterLength; i++) {
-    var charRandom = char[Math.floor(Math.random() * characterLength)]
-    passwordText.push(String.fromCharCode(char))
-  }
-  
+  let choice2 = prompt("Would you like to include uppercase letters?");
+
+    if (choice2 === "yes") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Added!");
+      } else {
+        alert("Not Added!");
+      }
+    } else if (choice2 === "no") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Not Added!");
+      } else {
+        alert("Added!");
+      }
+    } else  {
+      alert("Please input 'yes' or 'no'. Try Again!");
+    }
+
+   let choice3 = prompt("Would you like to include numbers?");
+
+    if (choice3 === "yes") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Added!");
+      } else {
+        alert("Not Added!");
+      }
+    } else if (choice3 === "no") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Not Added!");
+      } else {
+        alert("Added!");
+      }
+    } else {
+      alert("Please input 'yes' or 'no'. Try Again!");
+    }
+
+  let choice4 = prompt("Would you like to include special characters?");
+
+    if (choice4 === "yes") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Added!");
+      } else {
+        alert("Not Added!");
+      }
+    } else if (choice4 === "no") {
+      if(window.confirm("Press ok to confirm! otherwise press cancel")){
+      alert("Not Added!");
+      } else {
+        alert("Added!");
+      }
+    } else {
+      alert("Please input 'yes' or 'no'. Try Again!");
+    }
+
+    var includeLowercase = choice1.charLowercase;
+    var includeUppercase = choice2.charUppercase;
+    var includeNumbers = choice3.charNumbers;
+    var includeSpecialCharacters = choice4.charSpecialCharacters;
+
+    
+
 }
