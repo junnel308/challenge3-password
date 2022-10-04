6f9129e560647e45adf92915bccf0f265d2fad5a
junnel308 <junnelpadilla08@gmail.com>
2022-10-03 19:20:54 -0700

    added comments and added variation of 'y' and'n'

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 var charLowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -20,9 +20,12 @@ function writePassword() {
 
 }
 
+// Make prompts appear after clicking
 generateBtn.addEventListener("click", writePassword);
 
+//functions that included all of the conditions for the prompts.
 function generatePassword() {
+// prompts user to input in length of the password
 while (true) {
   passLength =  prompt("How many characters would you like to include? (Minimum of 8 character and Maximum of 128)");
   if(passLength >= 8 && passLength <= 128) {
@@ -37,9 +40,10 @@ while (true) {
     }
   }
 
+//prompts user to input if lowercase should be included
 while (true) {
-  choice1 = prompt("Would you like to include lowercase letters?   ( ' y/n ' )");
-    if (choice1 === "y") {
+  choice1 = prompt("Would you like to include lowercase letters?   (  y / n  )");
+    if (choice1 === "y" || choice1 === "Y") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to NOT add lowercase")){
         charLowercase = "abcdefghijklmnopqrstuvwxyz";
         alert("Lowercase characters added!");
@@ -49,7 +53,7 @@ while (true) {
         alert("Lowercase characters will NOT be added!");
         break;
       }
-    } else if (choice1 === "n") {
+    } else if (choice1 === "n" || choice1 === "N") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to add lowercase")){
         charLowercase = "";
         alert("Lowercase characters will NOT be added!");
@@ -64,9 +68,10 @@ while (true) {
     }
   }
 
+//prompts user to input if uppercase should be included
 while (true) {
-  choice2 = prompt("Would you like to include uppercase letters?   ( ' y/n ' )");
-    if (choice2 === "y") {
+  choice2 = prompt("Would you like to include uppercase letters?   (  y / n  )");
+    if (choice2 === "y" || choice2 === "Y") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to NOT add uppercase")){
         charUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         alert("Uppercase characters added!");
@@ -76,7 +81,7 @@ while (true) {
         alert("Uppercase will will NOT be added!");
         break;
       }
-    } else if (choice2 === "n") {
+    } else if (choice2 === "n" || choice2 === "N") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to add uppercase")){
         charUppercase = "";
         alert("Uppercase characters will NOT be added!");
@@ -91,9 +96,10 @@ while (true) {
     }
   }
 
+//prompts user to input if numbers should be included
 while (true) {
-   choice3 = prompt("Would you like to include numbers?   ( ' y/n ' )");
-    if (choice3 === "y") {
+   choice3 = prompt("Would you like to include numbers?   (  y / n  )");
+    if (choice3 === "y" || choice3 === "Y") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to NOT add numbers")){
         charNumbers = "0123456789";
         alert("Numbers added!");
@@ -103,7 +109,7 @@ while (true) {
         alert("Numbers will NOT be added!");
         break;
       }
-    } else if (choice3 === "n") {
+    } else if (choice3 === "n" || choice3 === "N") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to add numbers")){
         charNumbers = "";
         alert("Numbers will NOT be added!");
@@ -118,9 +124,10 @@ while (true) {
     }
   }
 
+//prompts user to input if special characters should be included
 while (true) {
-  choice4 = prompt("Would you like to include special characters?   ( ' y/n ' )");
-    if (choice4 === "y") {
+  choice4 = prompt("Would you like to include special characters?   (  y / n  )");
+    if (choice4 === "y" || choice4 === "Y") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to NOT add special characters")){
         charSpecialCharacters = "'!#$%&()*+,-./:;<=>?@[]^_`{|}~'";
         alert("Special Characters added!");
@@ -130,7 +137,7 @@ while (true) {
         alert("Special Characters will NOT be added!");
         break;
       }
-    } else if (choice4 === "n") {
+    } else if (choice4 === "n" || choice4 === "N") {
       if(window.confirm("Press 'Ok' to confirm! otherwise press 'Cancel' to add special characters")){
         charSpecialCharacters = "";
         alert("Special Characters will NOT be Added!");
@@ -145,6 +152,7 @@ while (true) {
     }
   }
 
+  //prompts user back to the first question if user input "n" in all criteria
     var max_count = 10;
     if(charLowercase + charUppercase + charNumbers + charSpecialCharacters === "") {
       alert("Must include at least 1 criteria. You will be re-prompted to the first question.");
@@ -152,14 +160,16 @@ while (true) {
       generatePassword();
     }
 
+  // variable that includes all the lowercase, uppercase, numbers and special characters
     var userChoiceCharacter =''.concat (charLowercase, charUppercase, charNumbers, charSpecialCharacters);
 
+  //This section chooses random characters that was chosen by the user and dispplay to the website
     var generatedPass = "";
 
     for (var i = 0; i < passLength; i++) {
       var randomPin = Math.floor(Math.random() * userChoiceCharacter.length);
       generatedPass += userChoiceCharacter.substring(randomPin, randomPin+1);
     }
-
+  // returns the value
   return generatedPass;
 }
